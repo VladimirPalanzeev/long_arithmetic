7287605f1ebc8577e7ca20cdf90a62541042feac
VladimirPalanzeev <palanzey@gmail.com>
2022-01-24 06:23:16 +0500

    [upd] Функция вывода зашифрованного числа

--- 01.py
+++ 01.py
@@ -7,5 +7,27 @@ def getData(a):
         d += a[i] * 10 ** i
     return d
 
+def printData(a):
+    for i in range(len(a) - 1, -1, -1):
+        print(f"{a[i]}", end="")
+    print()
+
+
 a = [3, 2, 1]
 print(getData(a))
+
+b = [54, 21, 67, 12, 54]
+print(getData(b))
+
+b = [4, 6, 9, 8, 5, 5]
+print(getData(b))
+
+print("Обычное число")
+a = [4, 6, 9, 8, 5, 5]
+printData(a)
+print(getData(a))
+print("\nЗашифрованное число")
+c = [54, 21, 67, 12, 54]
+printData(c)
+print(getData(c))
+
